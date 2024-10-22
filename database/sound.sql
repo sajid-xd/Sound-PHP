91fa0e43955195dadae2dd5ae3143d56f6039cfc
Sajid Ali <greatsajidali709@gmail.com>
2024-10-22 10:11:18 +0500

    update

--- database/sound.sql
+++ database/sound.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 15, 2024 at 04:46 PM
+-- Generation Time: Oct 22, 2024 at 07:10 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -112,7 +112,18 @@ CREATE TABLE `music` (
 --
 
 INSERT INTO `music` (`id`, `name`, `YEAR`, `ARTIST`, `ALBUM`, `isaudio`, `icon`, `soucre`) VALUES
-(1, '1.mp3', 2020, 1, 1, 1, 'aadat.jpg', '1.mp3');
+(1, 'test.mp3', 2020, 1, 1, 1, 'atirf1.jpeg', 'test.mp3'),
+(2, 'test.mp3', 2021, 3, 2, 1, '3.jpeg', 'test.mp3'),
+(4, '1.mp3', 2024, 2, 5, 1, '1.jpeg', '1.mp3'),
+(5, '1.mp3', 2022, 1, 3, 1, '3.jpeg', '1.mp3'),
+(6, '1.mp3', 2023, 3, 6, 1, '2.jpeg', '1.mp3'),
+(7, '1.mp3', 2022, 4, 6, 1, '4.jpeg', '1.mp3'),
+(8, 'test.mp3', 2020, 1, 1, 0, 'atirf1.jpeg', 'test.mp3'),
+(9, 'Rolling_in_the_Deep.mp3', 2021, 3, 2, 0, '3.jpeg', 'test.mp3'),
+(11, 'Shape_of_You.mp3', 2024, 2, 5, 0, '1.jpeg', 'test.mp3'),
+(12, 'Blinding_Lights.mp3', 2022, 1, 3, 0, '3.jpeg', 'test.mp3'),
+(13, 'Levitating.mp3', 2023, 3, 6, 0, '2.jpeg', 'test.mp3'),
+(14, 'Someone_You_Loved.mp3', 2022, 4, 6, 0, '4.jpeg', 'test.mp3');
 
 -- --------------------------------------------------------
 
@@ -281,7 +292,7 @@ ALTER TABLE `most_lis`
 -- AUTO_INCREMENT for table `music`
 --
 ALTER TABLE `music`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `trending_song`
